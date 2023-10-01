d0ca8456c7afc03f89cf577667655ecfa9940295
Jerry Chen <jhinresh@JerrydeMacBook-Pro.local>
2023-10-01 15:02:54 -0500

    first

---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PlaceDetails = ({ place }) => {
+    return (
+        <header>
+            <h1>{place.name}</h1>
+        </header>
+    );
+}
+export default PlaceDetails;
